a00922bc056dd9f7f54c19c3ae3c498ddda8cdfb
takuya kinoshita <kktak02@gmail.com>
2026-03-16 06:14:49 +0900

    updated test

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -3,6 +3,12 @@ import src.config.db_config as db_config
 
 
 def test_db_config_import():
+    env = os.getenv('APP_ENV')
     db_url = db_config.get_db_url()
-    assert db_url.database == "forex-test"
+    if env == 'dev':
+        assert db_url.database == 'forex'
+    elif env == 'test':
+        assert db_url.database == 'forex-test'
+    else:
+        assert db_url.database == 'forex'
 
