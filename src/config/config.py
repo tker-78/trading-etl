2fee8fcfb130624407d4221b312d46477cebb6ff
takuya kinoshita <kktak02@gmail.com>
2026-03-17 07:13:01 +0900

    finished t9

--- src/config/config.py
+++ src/config/config.py
@@ -1,3 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
 ### General setting params ###
 
 SCHEMA_NAME_OHLC = "ohlc"
@@ -7,48 +13,46 @@ SCHEMA_NAME_TICKER = "ticker"
 ### params for indicator setting ###
 
 RSI_TASK_DEFAULT_PARAMS = {
-    "period": 14,
-    "currency_pair_code": "USD/JPY",
-    "timeframe_code": "1m",
+    "period": int(os.getenv("DEFAULT_PERIOD", 14)),
+    "currency_pair_code": str(os.getenv("DEFAULT_CURRENCY_PAIR_CODE", "USD/JPY")),
+    "timeframe_code": str(os.getenv("DEFAULT_TIMEFRAME_CODE", "1m")),
 }
 
 RSI_FLOW_DEFAULT_PARAMS = {
-    "periods": [14, 28, 56],
-    "timeframes": ["1m", "5m", "30m", "1h", "4h"],
+    "periods": [int(period) for period in os.getenv("DEFAULT_PERIODS", "14,28,56").split(",")],
+    "timeframes": [str(timeframe) for timeframe in os.getenv("DEFAULT_TIMEFRAMES", "1m,5m,30m,1h,4h").split(",")],
 }
 
 SMA_TASK_DEFAULT_PARAMS = {
-    "period":  14,
-    "currency_pair_code": "USD/JPY",
-    "timeframe_code": "1m",
+    "period":  int(os.getenv("DEFAULT_PERIOD", 14)),
+    "currency_pair_code": str(os.getenv("DEFAULT_CURRENCY_PAIR_CODE", "USD/JPY")),
+    "timeframe_code": str(os.getenv("DEFAULT_TIMEFRAME_CODE", "1m"))
 }
 
 SMA_FLOW_DEFAULT_PARAMS = {
-    "periods": [14, 28, 56],
-    "timeframes": ["1m", "5m", "1h"],
+    "periods": [int(period) for period in os.getenv("DEFAULT_PERIODS", "14,28,56").split(",")],
+    "timeframes": [str(timeframe) for timeframe in os.getenv("DEFAULT_TIMEFRAMES", "1m,5m,30m,1h,4h").split(",")],
 }
 
 SMA_GOLDEN_CROSS_PARAMS = {
-    "short_period": 14,
-    "long_period": 28,
-    # "timeframe_code": "1m",
-    # "currency_pair_code": "USD/JPY",
+    "short_period": int(os.getenv("DEFAULT_SHORT_PERIOD", 14)),
+    "long_period": int(os.getenv("DEFAULT_LONG_PERIOD", 28)),
 }
 
 SMA_DEAD_CROSS_PARAMS = {
-    "short_period": 14,
-    "long_period": 28,
+    "short_period": int(os.getenv("DEFAULT_SHORT_PERIOD", 14)),
+    "long_period": int(os.getenv("DEFAULT_LONG_PERIOD", 28)),
 }
-
 
 
 EMA_TASK_DEFAULT_PARAMS = {
-    "period":  14,
-    "currency_pair_code": "USD/JPY",
-    "timeframe_code": "1m",
+    "period":  int(os.getenv("DEFAULT_PERIOD", 14)),
+    "currency_pair_code": str(os.getenv("DEFAULT_CURRENCY_PAIR_CODE", "USD/JPY")),
+    "timeframe_code": str(os.getenv("DEFAULT_TIMEFRAME_CODE", "1m"))
 }
 
 EMA_FLOW_DEFAULT_PARAMS = {
-    "periods": [14, 28, 56],
-    "timeframes": ["1m", "5m", "1h"],
+    "periods": [int(period) for period in os.getenv("DEFAULT_PERIODS", "14,28,56").split(",")],
+    "timeframes": [str(timeframe) for timeframe in os.getenv("DEFAULT_TIMEFRAMES", "1m,5m,30m,1h,4h").split(",")],
 }
+
